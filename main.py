f6d4db4ef778723f7843da0c374b83a293ab4bec
Rohan Gupta <rohangupta1994@gmail.com>
2018-11-22 01:37:32 -0500

    Added Ensemble classification and variious methods to analyze the results

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import pandas as pd
 import tensorflow as tf
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
 from tqdm import tqdm
 import math
 import matplotlib.pyplot
@@ -102,11 +106,6 @@ def loadUSPSData():
 
     return USPSMat, USPSTar
 
-
-### Sigmoid function
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
 ### Function for the Adding Bias Term
 def addBias(trainData, validData, testData):
     tempTrainData = trainData
@@ -128,8 +127,8 @@ def calculateLogisticOutput(theta, X):
     y = softmax(y)
     return y
 
-### Function for calculating Error
-def calculateAccuracy(y, t):
+### Function for calculating Accuracy using one-hot vector
+def calculateLogisticAccuracy(y, t):
     counter = 0
     for i in range(len(y)):
         if (np.argmax(y[i]) == t[i]):
@@ -138,28 +137,50 @@ def calculateAccuracy(y, t):
     acc = float(counter*100)/len(t)
     return acc
 
-#	Function for calculating Softmax
+###	Function for calculating Softmax
 def softmax(z):
-    exp = np.exp(z - np.max(z, axis=1).reshape((-1,1)))
+    exp = np.exp(z - np.max(z, axis=1).reshape((-1, 1)))
     norms = np.sum(exp, axis=1).reshape((-1,1))
     return exp / norms
 
-#	Function for converting Scalar to One Hot Vector
+###	Function for converting Scalar to One Hot Vector
 def oneHotEncode(targets):
-	return np.eye(NUM_CLASSES)[targets] 
+	return np.eye(NUM_CLASSES)[targets]  
+
+### Function for creating Confusion Matrix
+def createConfusionMatrix(targets, predictedTargets):
+    print("Confusion Matrix:")
+    confusionMat = confusion_matrix(targets, predictedTargets)
+    print(confusionMat)
+
+### Function for calculating Average Precision
+def calculateAveragePrecision(targets, predictedTargets):
+    y = label_binarize(targets, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y_pred = label_binarize(predictedTargets, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    precision = np.zeros(len(y))
+    recall = np.zeros(len(y))
+    average_precision = np.zeros(len(y))
+
+    for i in range(NUM_CLASSES):
+        #precision[i], recall[i], thresholds = precision_recall_curve(y[:, i], y_pred[:, i])
+        average_precision[i] = average_precision_score(y[:, i], y_pred[:, i])
+
+    #precision_micro, recall_micro, thresholds = precision_recall_curve(y.ravel(), y_pred.ravel())
+    average_precision_micro = average_precision_score(y, y_pred, average="micro")
+    print('Average Precision Score (micro-averaged over all classes): ' + str(average_precision_micro))
+    print("\n")
 
 ### Function for performing Logistic Regression
-def performMulticlassLogisticRegression():
+def performMulticlassLogisticRegression(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar):
     ### Setting Hyperparameters
     EPOCHS = 1500
     ETA = 0.05
+    BATCH_SIZE = 256
 
     trainAcc = []
     validAcc = []
     testAcc = []
-
-    trainData, validData, testData, trainTarget, validTarget, testTarget = loadMNISTData()
-    USPSMat, USPSTar = loadUSPSData()
 
     oneHotTarget = oneHotEncode(trainTarget.transpose()).reshape(-1, 10)
 
@@ -170,57 +191,56 @@ def performMulticlassLogisticRegression():
     theta = 5 * theta
     print("Weights shape: ", theta.shape)
 
-    ### Performing Stochastic Gradient Descent
+    ### Performing Mini-batch Stochastic Gradient Descent
     for i in tqdm(range(EPOCHS)):
-        z = np.dot(trainData, theta)
-        #a = sigmoid(z)
-        a = softmax(z)
 
-        gradient = np.dot(np.transpose(np.subtract(a, oneHotTarget)), trainData) / trainTarget.size
-        gradient = gradient.transpose()
-        eta_gradient = ETA * gradient
-        theta = np.subtract(theta, eta_gradient)
+        ### Starting Batch Training
+        for start in range(0, len(trainData), BATCH_SIZE):
+            end = start + BATCH_SIZE
+            z = np.dot(trainData[start:end], theta)
+            a = softmax(z)
+
+            gradient = np.dot(np.transpose(np.subtract(a, oneHotTarget[start:end])), trainData[start:end]) / BATCH_SIZE #trainTarget.size
+            gradient = gradient.transpose()
+            eta_gradient = ETA * gradient
+            theta = np.subtract(theta, eta_gradient)
 
         trainY = calculateLogisticOutput(theta, trainData)
-        tempAcc = calculateAccuracy(trainY, trainTarget)
+        tempAcc = calculateLogisticAccuracy(trainY, trainTarget)
         trainAcc.append(tempAcc)
 
         validY = calculateLogisticOutput(theta, validData)
-        tempAcc = calculateAccuracy(validY, validTarget)
+        tempAcc = calculateLogisticAccuracy(validY, validTarget)
         validAcc.append(tempAcc)
 
         if (i%100 == 0):
         	print("Iteration: " + str(i) + ", Training Acc = " + str(trainAcc[i]) + ", Validation Acc = " + str(validAcc[i]))
 
-    print("=========Results=========")
+    print("=========Results for Logistic=========")
 
-    testY = calculateLogisticOutput(theta, testData)
-    testAcc = calculateAccuracy(testY, testTarget)
-    print("Testing Accuracy (MNIST) = ", str(testAcc))
+    predictedMNISTTarget = calculateLogisticOutput(theta, testData)
+    testAcc = calculateLogisticAccuracy(predictedMNISTTarget, testTarget)
+    print("MNIST Testing Accuracy: ", str(testAcc))
 
-    testUSPSY = calculateLogisticOutput(theta, USPSMat)
-    testUSPSAcc = calculateAccuracy(testUSPSY, USPSTar)
-    print("Testing Accuracy (USPS) = ", str(testUSPSAcc))
+    predictedUSPSTarget = calculateLogisticOutput(theta, USPSMat)
+    testUSPSAcc = calculateLogisticAccuracy(predictedUSPSTarget, USPSTar)
+    print("USPS Testing Accuracy: ", str(testUSPSAcc))
 
-    '''
-    print ('-----Logistic Regression Performance using Stochastic Gradient Descent-----')
-    print ("Dataset Type            = " + datasetType)
-    print ("Feature Setting         = " + featureSetting)
-    print ("Hyperparameters: eta = " + str(eta) + ", epochs = " + str(epochs))
-    print ("Erms Training           = " + str(np.around(min(trainErms),5)))
-    print ("Accuracy Training       = " + str(np.around(max(trainAcc),2)) + "%")
-    print ("Erms Validation         = " + str(np.around(min(validErms),5)))
-    print ("Accuracy validation     = " + str(np.around(max(validAcc),2)) + "%")
-    print ("Erms Testing            = " + str(np.around(min(testErms),5)))
-    print ("Accuracy Testing        = " + str(np.around(max(testAcc),2)) + "%")
-    '''
+    createConfusionMatrix(testTarget, np.argmax(predictedMNISTTarget, axis=1))
+    calculateAveragePrecision(testTarget, np.argmax(predictedMNISTTarget, axis=1))
+
+    createConfusionMatrix(USPSTar, np.argmax(predictedUSPSTarget, axis=1))
+    calculateAveragePrecision(USPSTar, np.argmax(predictedUSPSTarget, axis=1))
+    
+    return predictedMNISTTarget, predictedUSPSTarget
 
 
-# Initializing the weights to Normal Distribution
+### Initializing the weights to Normal Distribution
 def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
 
-def calculateNeuralAccuracy(y, t):
+### Function for calculating Accuracy
+def calculateAccuracy(y, t):
     counter = 0
     for i in range(len(y)):
         if (y[i] == t[i]):
@@ -230,23 +250,20 @@ def calculateNeuralAccuracy(y, t):
     return acc
 
 ### Function for performing Neural Networks
-def performNeuralNetwork():
+def performNeuralNetwork(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar):
     # Setting Hyperparameters
-    EPOCHS = 500
-    LEARNING_RATE = 0.01
+    EPOCHS = 400
+    LEARNING_RATE = 0.025
     BATCH_SIZE = 256
 
     trainAcc = []
     validAcc = []
     testAcc = []
 
-    trainData, validData, testData, trainTarget, validTarget, testTarget = loadMNISTData()
-    USPSMat, USPSTar = loadUSPSData()
-
     oneHotTrainTarget = oneHotEncode(trainTarget.transpose()).reshape(-1, 10)
     oneHotValidTarget = oneHotEncode(validTarget.transpose()).reshape(-1, 10)
 
-    NUM_HIDDEN_NEURONS_LAYER_1 = 256
+    NUM_HIDDEN_NEURONS_LAYER_1 = 512
     #NUM_HIDDEN_NEURONS_LAYER_2 = 100
 
     ### Defining Placeholder
@@ -306,71 +323,113 @@ def performNeuralNetwork():
                     predictedTestTarget = sess.run(prediction, feed_dict={inputTensor: testData})
                     predictedUSPSTarget = sess.run(prediction, feed_dict={inputTensor: USPSMat})
 
-                    accMNIST = calculateNeuralAccuracy(predictedTestTarget, testTarget)
+                    accMNIST = calculateAccuracy(predictedTestTarget, testTarget)
                     print("MNIST Testing Accuracy: " + str(np.around(accMNIST, 2)) + "%")
 
-                    accUSPS = calculateNeuralAccuracy(predictedUSPSTarget, USPSTar)
+                    accUSPS = calculateAccuracy(predictedUSPSTarget, USPSTar)
                     print("USPS Testing Accuracy: " + str(np.around(accUSPS, 2)) + "%")
             
         # Testing
-        predictedTestTarget = sess.run(prediction, feed_dict={inputTensor: testData})
+        predictedMNISTTarget = sess.run(prediction, feed_dict={inputTensor: testData})
         predictedUSPSTarget = sess.run(prediction, feed_dict={inputTensor: USPSMat})
 
+    print("=========Results for Neural=========")
     print("MNIST Training Accuracy: " + str(np.around(trainAcc[EPOCHS-1]*100, 2)) + "%")
     print("MNIST Validation Accuracy: " + str(np.around(validAcc[EPOCHS-1]*100, 2)) + "%")
 
-    accMNIST = calculateNeuralAccuracy(predictedTestTarget, testTarget)
+    accMNIST = calculateAccuracy(predictedMNISTTarget, testTarget)
     print("MNIST Testing Accuracy: " + str(np.around(accMNIST, 2)) + "%")
 
-    accUSPS = calculateNeuralAccuracy(predictedUSPSTarget, USPSTar)
+    accUSPS = calculateAccuracy(predictedUSPSTarget, USPSTar)
     print("USPS Testing Accuracy: " + str(np.around(accUSPS, 2)) + "%")
+
+    createConfusionMatrix(testTarget, predictedMNISTTarget)
+    calculateAveragePrecision(testTarget, predictedMNISTTarget)
+
+    createConfusionMatrix(USPSTar, predictedUSPSTarget)
+    calculateAveragePrecision(USPSTar, predictedUSPSTarget)
+    
+    return predictedMNISTTarget, predictedUSPSTarget
 
 
 ### Function for performing Support Vector Machine
-def performSVM():
-    trainData, validData, testData, trainTarget, validTarget, testTarget = loadMNISTData()
-    USPSMat, USPSTar = loadUSPSData()
-
-    #trainData = trainData[:100]
-    #trainTarget = trainTarget[:100]
-
-    clf = svm.SVC(C=1.0, kernel='linear', gamma='auto', decision_function_shape='ovr')
+def performSVM(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar):
+    clf = svm.SVC(C=1.0, kernel='rbf', gamma='auto', decision_function_shape='ovr')
     clf.fit(trainData, np.ravel(trainTarget))
 
-    predictedTarget = clf.predict(testData)
+    predictedMNISTTarget = clf.predict(testData)
+    predictedUSPSTarget = clf.predict(USPSMat)
 
+    print("=========Results for SVM=========")
     print("MNIST Validation Accuracy: " + str(clf.score(validData, validTarget)*100))
     print("MNIST Testing Accuracy: " + str(clf.score(testData, testTarget)*100))
     print("USPS Testing Accuracy: " + str(clf.score(USPSMat, USPSTar)*100))
-    return
 
-def performRandomForest():
-    trainData, validData, testData, trainTarget, validTarget, testTarget = loadMNISTData()
-    USPSMat, USPSTar = loadUSPSData()
+    createConfusionMatrix(testTarget, predictedMNISTTarget)
+    calculateAveragePrecision(testTarget, predictedMNISTTarget)
 
-    #trainData = trainData[:100]
-    #trainTarget = trainTarget[:100]
+    createConfusionMatrix(USPSTar, predictedUSPSTarget)
+    calculateAveragePrecision(USPSTar, predictedUSPSTarget)
 
+    return predictedMNISTTarget, predictedUSPSTarget
+
+def performRandomForest(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar):
     clf = RandomForestClassifier(n_estimators=1000)
     clf.fit(trainData, np.ravel(trainTarget))
 
+    predictedMNISTTarget = clf.predict(testData)
+    predictedUSPSTarget = clf.predict(USPSMat)
+
+    print("=========Results for Random Forest=========")
     print("MNIST Validation Accuracy: " + str(clf.score(validData, validTarget)*100))
     print("MNIST Testing Accuracy: " + str(clf.score(testData, testTarget)*100))
     print("USPS Testing Accuracy: " + str(clf.score(USPSMat, USPSTar)*100))
-    return
+
+    createConfusionMatrix(testTarget, predictedMNISTTarget)
+    calculateAveragePrecision(testTarget, predictedMNISTTarget)
+
+    createConfusionMatrix(USPSTar, predictedUSPSTarget)
+    calculateAveragePrecision(USPSTar, predictedUSPSTarget)
+
+    return predictedMNISTTarget, predictedUSPSTarget
+
+def performVotingClassification(targets, logisticTarget, neuralTarget, svmTarget, randomForestTarget):
+    voteMat = np.zeros(10)
+    ensembleTarget = []
+
+    for i in range(len(targets)):
+        voteMat[np.argmax(logisticTarget[i])] += 1
+        voteMat[neuralTarget[i]] += 1
+        voteMat[svmTarget[i]] += 1
+        voteMat[randomForestTarget[i]] += 1
+        maxi = np.argmax(voteMat)
+
+        if(voteMat[maxi] == 1):
+            maxi = neuralTarget[i]
+
+        ensembleTarget.append(maxi)
+        #print(voteMat, neuralTarget[i], ensembleTarget[i], USPSTar[i])
+        voteMat = np.zeros(10)
+
+    print("=========Results for Ensemble (Majority Voting)=========")
+    acc = calculateAccuracy(ensembleTarget, targets)
+    print("Ensemble Accuracy: " + str(np.around(acc, 2)) + "%")
+
+    createConfusionMatrix(targets, ensembleTarget)
+    calculateAveragePrecision(targets, ensembleTarget)
+   
 
 
+trainData, validData, testData, trainTarget, validTarget, testTarget = loadMNISTData()
+USPSMat, USPSTar = loadUSPSData()
 
-#performMulticlassLogisticRegression()
+logisticMNISTTar, logisticUSPSTar = performMulticlassLogisticRegression(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar)
+neuralMNISTTar, neuralUSPSTar = performNeuralNetwork(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar) 
+svmMNISTTar, svmUSPSTar = performSVM(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar)
+randomForestMNISTTar, randomForestUSPSTar = performRandomForest(trainData, validData, testData, trainTarget, validTarget, testTarget, USPSMat, USPSTar)
 
-#performNeuralNetwork()
-
-performSVM()
-
-#performRandomForest()
-
-
-
+performVotingClassification(testTarget, logisticMNISTTar, neuralMNISTTar, svmMNISTTar, randomForestMNISTTar)
+performVotingClassification(USPSTar, logisticUSPSTar, neuralUSPSTar, svmUSPSTar, randomForestUSPSTar)
 
 
 
